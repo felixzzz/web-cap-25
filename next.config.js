968f0c7dc852f2e782be798e691c1ae710072c0c
rzlzrdf <rizallazu76@gmail.com>
2026-01-26 14:44:53 +0700

    feat: Add `localhost:8081` to Next.js image domains and reorder `localhost:8000`.

--- next.config.js
+++ next.config.js
@@ -47,15 +47,15 @@ const nextConfig = {
         pathname: "**",
       },
       {
-        protocol: "http",
-        hostname: "localhost",
-        port: "8000",
-        pathname: "**",
-      },
-      {
         protocol: "https",
         hostname: "dev-back.chandra-asri.com",
         port: "",
+        pathname: "**",
+      },
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "8000",
         pathname: "**",
       },
       {
