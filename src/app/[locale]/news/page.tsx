063ae57885551c8bc6c3e17c48c2913ce0a5c3de
rzlzrdf <rizallazu76@gmail.com>
2026-01-28 11:11:22 +0700

    feat: fetch news page data and set page ID using `PageIdSetter` component.

--- src/app/[locale]/news/page.tsx
+++ src/app/[locale]/news/page.tsx
@@ -1,10 +1,16 @@
 import Navbar from "@/components/global/Navbar"
 import NewsHeader from "./_components/NewsHeader"
 import NewsAndPressReleases from "./_components/NewsAndPressReleases"
-import { PaginationHandlerResponse } from "@/lib/types"
+import { HttpGeneralResponse, PaginationHandlerResponse } from "@/lib/types"
 import { MetaDocumentItem, Post, PostCategory } from "@/lib/fragment"
-import { getDocuments, getPostCategories, getPostList } from "@/lib/api"
+import {
+  getDocuments,
+  getPage,
+  getPostCategories,
+  getPostList,
+} from "@/lib/api"
 import { Suspense } from "react"
+import { PageIdSetter } from "@/components/providers/query-provider"
 
 export const revalidate = 120
 
@@ -31,12 +37,15 @@ export default async function NewsPage({
   const blogs: PaginationHandlerResponse<Post[]> = await getPostList(
     `?limit=12&lang=id&page=1&sort=published_at&order=DESC&type=blog&page=${searchParams.page || 1}&${searchParams["categories[]"] ? `categories%5B%5D=${searchParams["categories[]"] || ""}` : ""}`
   )
+  const data: HttpGeneralResponse<unknown> = await getPage("news")
+
   const featuredPost = [news?.data?.[0], blogs?.data?.[0]]
 
   return (
     <>
       <div className="mt-16">
         <Navbar isBackgroundWhite />
+        {data?.id && <PageIdSetter id={data.id.toString()} />}
         {featuredPost && (
           <NewsHeader tab={searchParams.tab} featuredPost={featuredPost} />
         )}
